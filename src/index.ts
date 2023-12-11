f09770342af4c229c24cdf9614fa2444c29d2105
Drew Miller <49833875+wwdrew@users.noreply.github.com>
2023-12-11 17:17:27 +0000

    feat: add initial dummy authenticate function

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,5 @@
-import {
-  NativeModulesProxy,
-  EventEmitter,
-  Subscription,
-} from "expo-modules-core";
-
-// Import the native module. On web, it will be resolved to ExpoSpotifySDK.web.ts
-// and on native platforms to ExpoSpotifySDK.ts
-import {
-  ChangeEventPayload,
-  ExpoSpotifySDKViewProps,
-} from "./ExpoSpotifySDK.types";
 import ExpoSpotifySDKModule from "./ExpoSpotifySDKModule";
 
-// Get the native constant value.
-export const PI = ExpoSpotifySDKModule.PI;
-
-export function hello(): string {
-  return ExpoSpotifySDKModule.hello();
+export function authenticatePrompt(): string {
+  return ExpoSpotifySDKModule.authenticatePrompt();
 }
-
-export async function setValueAsync(value: string) {
-  return await ExpoSpotifySDKModule.setValueAsync(value);
-}
-
-const emitter = new EventEmitter(
-  ExpoSpotifySDKModule ?? NativeModulesProxy.ExpoSpotifySDK,
-);
-
-export function addChangeListener(
-  listener: (event: ChangeEventPayload) => void,
-): Subscription {
-  return emitter.addListener<ChangeEventPayload>("onChange", listener);
-}
-
-export { ExpoSpotifySDKViewProps, ChangeEventPayload };
