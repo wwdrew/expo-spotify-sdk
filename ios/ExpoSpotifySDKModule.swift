c5deb5640fbea2d6faba6ac2fb36da2fae69855b
Drew Miller <49833875+wwdrew@users.noreply.github.com>
2024-01-15 17:35:11 +0000

    pass in new config to native function

--- ios/ExpoSpotifySDKModule.swift
+++ ios/ExpoSpotifySDKModule.swift
@@ -2,19 +2,25 @@ import ExpoModulesCore
 import SpotifyiOS
 
 public class ExpoSpotifySDKModule: Module {
-
-
+    
+    
     public func definition() -> ModuleDefinition {
-
+        
         let spotifySession = ExpoSpotifySessionManager.shared
-
+        
         Name("ExpoSpotifySDK")
-
+        
         Function("isAvailable") {
             return spotifySession.spotifyAppInstalled()
         }
-
-        AsyncFunction("authenticateAsync") { (scopes: [String], promise: Promise) in
+        
+        AsyncFunction("authenticateAsync") { (config: [String: Any], promise: Promise) in
+            guard let clientId = config["clientId"] as? String,
+                  let scopes = config["scopes"] as? [String] else {
+                promise.reject("INVALID_CONFIG", "Invalid SpotifyConfig object")
+                return
+            }
+            
             spotifySession.authenticate(requestedScopes: scopes).done { session in
                 let sessionData: [String: Any] = [
                     "accessToken": session.accessToken,
