d5052971484b75bae37285ad2811f3b69cd69b4c
Drew Miller <49833875+wwdrew@users.noreply.github.com>
2024-01-15 11:13:22 +0000

    fix: don't accept handling url unless it has been

--- ios/ExpoSpotifyAuthDelegate.swift
+++ ios/ExpoSpotifyAuthDelegate.swift
@@ -3,9 +3,11 @@ import SpotifyiOS
 
 public class ExpoSpotifyAuthDelegate: ExpoAppDelegateSubscriber {
     let sessionManager = ExpoSpotifySessionManager.shared
-
+    
     public func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-        sessionManager.sessionManager?.application(app, open: url, options: options)
-        return true
+        if let canHandleURL = sessionManager.sessionManager?.application(app, open: url, options: options) {
+            return canHandleURL
+        }
+        return false
     }
 }
