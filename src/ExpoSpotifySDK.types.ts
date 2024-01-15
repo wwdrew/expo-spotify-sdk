12b2126596985b247687d1044874018b814cfbd7
Drew Miller <49833875+wwdrew@users.noreply.github.com>
2024-01-15 10:40:47 +0000

    refactor: improve types

--- src/ExpoSpotifySDK.types.ts
+++ src/ExpoSpotifySDK.types.ts
@@ -1,3 +1,26 @@
-export type ChangeEventPayload = {
-  value: string;
-};
+export interface SpotifySession {
+  accessToken: string;
+  refreshToken: string;
+  expirationDate: number;
+  isExpired: boolean;
+}
+
+export type SpotifyScope =
+  | "ugc-image-upload"
+  | "user-read-playback-state"
+  | "user-modify-playback-state"
+  | "user-read-currently-playing"
+  | "app-remote-control"
+  | "streaming"
+  | "playlist-read-private"
+  | "playlist-read-collaborative"
+  | "playlist-modify-private"
+  | "playlist-modify-public"
+  | "user-follow-modify"
+  | "user-follow-read"
+  | "user-top-read"
+  | "user-read-recently-played"
+  | "user-library-modify"
+  | "user-library-read"
+  | "user-read-email"
+  | "user-read-private";
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,11 @@
+import { SpotifyScope, SpotifySession } from "./ExpoSpotifySDK.types";
 import ExpoSpotifySDKModule from "./ExpoSpotifySDKModule";
-
-interface SpotifySession {
-  accessToken: string;
-  refreshToken: string;
-  expirationDate: number;
-  isExpired: boolean;
-}
 
 function isAvailable(): boolean {
   return ExpoSpotifySDKModule.isAvailable();
 }
 
-function authenticateAsync(scopes: string[]): Promise<SpotifySession> {
+function authenticateAsync(scopes: SpotifyScope[]): Promise<SpotifySession> {
   return ExpoSpotifySDKModule.authenticate(scopes);
 }
 
