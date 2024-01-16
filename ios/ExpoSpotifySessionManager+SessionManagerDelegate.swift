0f0186bf6f6d4f1ee2b0cbdbf71e641a8660cd09
Drew Miller <49833875+wwdrew@users.noreply.github.com>
2024-01-16 17:58:45 +0000

    feat: implement flag to toggle whether to send code or session on authentication (#2)

--- ios/ExpoSpotifySessionManager+SessionManagerDelegate.swift
+++ ios/ExpoSpotifySessionManager+SessionManagerDelegate.swift
@@ -3,14 +3,24 @@ import SpotifyiOS
 
 extension ExpoSpotifySessionManager: SPTSessionManagerDelegate {
     public func sessionManager(manager _: SPTSessionManager, didInitiate session: SPTSession) {
-        authPromiseSeal?.fulfill(session)
+        if shouldRequestAccessToken {
+            authPromiseSeal?.fulfill(AuthenticationResult.session(session))
+        }
     }
-
+    
     public func sessionManager(manager _: SPTSessionManager, didFailWith error: Error) {
         authPromiseSeal?.reject(error)
     }
-
+    
     public func sessionManager(manager _: SPTSessionManager, didRenew session: SPTSession) {
-        authPromiseSeal?.fulfill(session)
+        authPromiseSeal?.fulfill(AuthenticationResult.session(session))
+    }
+    
+    public func sessionManager(manager: SPTSessionManager, shouldRequestAccessTokenWith code: String) -> Bool {
+        if !shouldRequestAccessToken {
+            authPromiseSeal?.fulfill(AuthenticationResult.authorizationCode(code))
+        }
+        
+        return shouldRequestAccessToken
     }
 }
