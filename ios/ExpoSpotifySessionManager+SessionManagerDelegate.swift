8dd51b239119fd07800c1be683bcadb1c6b1ea4f
Drew Miller <49833875+wwdrew@users.noreply.github.com>
2024-01-16 12:08:22 +0000

    don't set swap or refresh values unless they exist

--- ios/ExpoSpotifySessionManager+SessionManagerDelegate.swift
+++ ios/ExpoSpotifySessionManager+SessionManagerDelegate.swift
@@ -13,4 +13,10 @@ extension ExpoSpotifySessionManager: SPTSessionManagerDelegate {
     public func sessionManager(manager _: SPTSessionManager, didRenew session: SPTSession) {
         authPromiseSeal?.fulfill(session)
     }
+
+    public func sessionManager(manager: SPTSessionManager, shouldRequestAccessTokenWith code: String) -> Bool {
+        // Return true if you want the session manager to request an access token with the authorization code.
+        // Otherwise, return false.
+        return false
+    }
 }
