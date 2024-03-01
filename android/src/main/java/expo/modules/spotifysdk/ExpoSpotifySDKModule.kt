0420f9cae5eaaf60f30d5dca07d48bd50feaf6e4
Drew Miller <49833875+wwdrew@users.noreply.github.com>
2024-03-01 12:05:45 +0000

    implement isAvailable function

--- android/src/main/java/expo/modules/spotifysdk/ExpoSpotifySDKModule.kt
+++ android/src/main/java/expo/modules/spotifysdk/ExpoSpotifySDKModule.kt
@@ -1,17 +1,23 @@
 package expo.modules.spotifysdk
 
+import android.content.pm.PackageManager
 import expo.modules.kotlin.Promise
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 
 class ExpoSpotifySDKModule : Module() {
 
+  private val context
+    get() = requireNotNull(appContext.reactContext)
+
   override fun definition() = ModuleDefinition {
 
     Name("ExpoSpotifySDK")
 
     Function("isAvailable") {
-      return@Function false
+      val packageManager: PackageManager = context.packageManager
+      val intent = packageManager.getLaunchIntentForPackage("com.spotify.music")
+      return@Function intent != null
     }
 
     AsyncFunction("authenticateAsync") { config: String, promise: Promise ->
