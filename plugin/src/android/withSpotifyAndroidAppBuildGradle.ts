f62ecaa1ba41ffc91940afbe5770af492574a9b6
Drew Miller <49833875+wwdrew@users.noreply.github.com>
2024-03-01 15:16:40 +0000

    update plugin to insert redirect uri and client id from config

--- plugin/src/android/withSpotifyAndroidAppBuildGradle.ts
+++ plugin/src/android/withSpotifyAndroidAppBuildGradle.ts
@@ -8,13 +8,18 @@ export const withSpotifyAndroidAppBuildGradle: ConfigPlugin<SpotifyConfig> = (
 ) => {
   return withAppBuildGradle(config, (config) => {
     const defaultConfigPattern = /(defaultConfig\s*{[\s\S]*?)(})/s;
-    const manifestPlaceholders = `manifestPlaceholders = [redirectSchemeName: "${spotifyConfig.scheme}", redirectHostName: "${spotifyConfig.host}"]`;
+    const manifestPlaceholders = `
+        buildConfigField "String", "SPOTIFY_CLIENT_ID", "${spotifyConfig.clientID}"
+        buildConfigField "String", "SPOTIFY_REDIRECT_URI", "${spotifyConfig.scheme}://${spotifyConfig.host}"
+
+        manifestPlaceholders = [redirectSchemeName: "${spotifyConfig.scheme}", redirectHostName: "${spotifyConfig.host}"]
+    `;
 
     if (defaultConfigPattern.test(config.modResults.contents)) {
       // If the defaultConfig block exists, add the manifestPlaceholders to it
       config.modResults.contents = config.modResults.contents.replace(
         defaultConfigPattern,
-        `$1\n${manifestPlaceholders}\n$2`,
+        `$1${manifestPlaceholders}$2`,
       );
     } else {
       // If the defaultConfig block doesn't exist, add it to the android block
